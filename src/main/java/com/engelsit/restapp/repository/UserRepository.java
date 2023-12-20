4200024360bf724a004f55ff6c990e9ed6a3b98f
Raphael <info@engels-it.com>
2023-12-20 22:20:18 +0100

    added Optional findByEmailAndPassword to validate whether a user with a specific email and password exists.

--- src/main/java/com/engelsit/restapp/repository/UserRepository.java
+++ src/main/java/com/engelsit/restapp/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.engelsit.restapp.repository;
 import com.engelsit.restapp.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
