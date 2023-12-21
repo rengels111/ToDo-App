2447f1d6c84c569c26215758ae680042c12d69f3
Raphael <info@engels-it.com>
2023-12-21 11:47:20 +0100

    added findAllByUserId method to filter todos by userId

--- src/main/java/com/engelsit/restapp/repository/ToDoRepository.java
+++ src/main/java/com/engelsit/restapp/repository/ToDoRepository.java
@@ -3,5 +3,10 @@ package com.engelsit.restapp.repository;
 import com.engelsit.restapp.entity.ToDo;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface ToDoRepository extends CrudRepository<ToDo, Integer> {
+
+    Set<ToDo> findAllByUserId(int userId);
+
 }
