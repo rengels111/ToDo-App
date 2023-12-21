901fd4adea1ccc360006d2ad7625c32cf0aa3689
Raphael <info@engels-it.com>
2023-12-21 11:51:20 +0100

    autowired toDoRepository. created randomUUID for secretKey when user will be registered. Added method getAllUsers() to find all users with their todos. Added validation if email and password matches user. Added delete(...)-method to delete user by id.

--- src/main/java/com/engelsit/restapp/controller/UserController.java
+++ src/main/java/com/engelsit/restapp/controller/UserController.java
@@ -1,24 +1,31 @@
 package com.engelsit.restapp.controller;
 
 import com.engelsit.restapp.entity.User;
+import com.engelsit.restapp.repository.ToDoRepository;
 import com.engelsit.restapp.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class UserController {
 
     private final UserRepository userRepository;
+    private final ToDoRepository toDoRepository;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, ToDoRepository toDoRepository) {
         this.userRepository = userRepository;
+        this.toDoRepository = toDoRepository;
     }
 
     @PostMapping("/register")
     private ResponseEntity<User> register(@RequestBody User newUser) {
+        // generate secretKey:
+        newUser.setSecretKey(UUID.randomUUID().toString());
+
         var registeredUser = userRepository.save(newUser);
         return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
     }
@@ -33,12 +40,36 @@ public class UserController {
         return new ResponseEntity("User not found with id " + id, HttpStatus.NOT_FOUND);
     }
 
+    @GetMapping("/user/all")
+    private ResponseEntity<Iterable<User>> getAllUsers() {
+        Iterable<User> allUsers = userRepository.findAll();
+
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
     @GetMapping("/validate")
-    private boolean validate(@RequestParam(value = "email") String email,
+    private ResponseEntity<String> validate(@RequestParam(value = "email") String email,
                                           @RequestParam(value = "password") String password) {
 
         var validUser = userRepository.findByEmailAndPassword(email, password);
 
-        return validUser.isPresent();
+        if (validUser.isPresent()) {
+            return new ResponseEntity<>("API secret: " + validUser.get().getSecretKey(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Wrong credential / no account found", HttpStatus.NOT_FOUND);
+    }
+
+
+    // In progress. I have to find a way how to delete the todos of this user first.
+    @DeleteMapping("/user")
+    private ResponseEntity<User> delete(@RequestParam(value = "id") int id) {
+        Optional<User> userInDb = userRepository.findById(id);
+
+        if (userInDb.isPresent()) {
+            toDoRepository.deleteAll(userInDb.get().getTodos());
+            userRepository.deleteById(id);
+            return new ResponseEntity("User deleted with id " + id, HttpStatus.OK);
+        }
+        return new ResponseEntity("User not found with id " + id, HttpStatus.NOT_FOUND);
     }
 }
